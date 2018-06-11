1e139b9171147611a3489252f0d21742cdb7613f
Jonathan Gomez <gomez.jonathan.06@gmail.com>
2018-06-11 19:01:24 -0300

    eventos
    se agregaron mas estilos

--- src/app/eventos/eventos.component.ts
+++ src/app/eventos/eventos.component.ts
@@ -10,11 +10,11 @@ import {FormControl} from '@angular/forms';
 })
 export class EventosComponent implements OnInit {
 
-  agenda: Observable<Actividad[]>=[
-    new Actividad('9:00','9:15','Sensor de Nivel de Agua en Ruta','Grupo ADTD UTN presenta sensor arduino para medir el agua sobre las baquinas y realizar prevencion','COFFEE MEET','15 para presentarse con el asistente alegido'),
-    new Actividad('9:00','9:15','Sensor de Nivel de Agua en Ruta','Grupo ADTD UTN presenta sensor arduino para medir el agua sobre las baquinas y realizar prevencion','COFFEE MEET','15 para presentarse con el asistente alegido'),
-    new Actividad('9:00','9:15','Sensor de Nivel de Agua en Ruta','Grupo ADTD UTN presenta sensor arduino para medir el agua sobre las baquinas y realizar prevencion','COFFEE MEET','15 para presentarse con el asistente alegido'),
-    new Actividad('9:00','9:15','Sensor de Nivel de Agua en Ruta','Grupo ADTD UTN presenta sensor arduino para medir el agua sobre las baquinas y realizar prevencion','COFFEE MEET','15 para presentarse con el asistente alegido'),
+  agenda: Actividad[]=[
+    new Actividad('9:00','9:15','','','COFFEE MEET','15´ para presentarse con el asistente elegido'),
+    new Actividad('9:30','9:15','Sensor de Nivel de Agua en Ruta','Grupo ADTD UTN presenta sensor arduino para medir el agua sobre las baquinas y realizar prevencion','CONGRESS','Disertación a cargo de influenciadores'),
+    new Actividad('10:30','9:15','Sensor de Nivel de Agua en Ruta','Grupo ADTD UTN presenta sensor arduino para medir el agua sobre las baquinas y realizar prevencion','WORKSHOP','Resolución de problemáticas junto al panel de expertos'),
+    new Actividad('13:30','9:15','Sensor de Nivel de Agua en Ruta','Grupo ADTD UTN presenta sensor arduino para medir el agua sobre las baquinas y realizar prevencion','SUMMIT','Espacio de 10´ para presentar tu idea o prototipo a posibles socios clave'),
   ]
 
   constructor() { }
