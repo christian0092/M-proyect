00691db50960d841c4e5d6ccd38c055737e6cca2
christian <christian0092@hotmail.com>
2018-06-14 22:00:08 -0300

    Revert "Modal M-Coffee"
    This reverts commit 5472873a4e6bd74cbd9d3766cc68e37e1de4f0e8.

--- src/app/perfil/modal-coffee.component.ts
+++ src/app/perfil/modal-coffee.component.ts
@@ -1,35 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Participant } from '../models/participant';
-import {Observable} from 'rxjs/Observable';
-import {FormControl} from '@angular/forms';
-
-
 
 @Component({
-  selector: 'modal-coffee',
+  selector: 'app-modal-coffee',
   templateUrl: './modal-coffee.component.html',
   styleUrls: ['./modal-coffee.component.css']
 })
 export class ModalCoffeeComponent implements OnInit {
-	ParticipantList:Participant[]=[
-    new Participant('Christian Molina',0),
-    new Participant('Fernanda Micakosky',1),
-    new Participant('Jonathan Gomez',2),
-    new Participant('Angeles Perez Angueria',2),
-    new Participant('La señora de España',0),
-    new Participant('Mel Gibson',0),
-    new Participant('El retutu papa',1),
-    new Participant('Completar cansa',2),
-    new Participant('Jhon wick',1),
- 	]
- 	Show:boolean=false;
-  constructor() { 
-   console.log(JSON.stringify(this.ParticipantList)); }
+
+  constructor() { }
 
   ngOnInit() {
-  }
-  displayshow(){
-  	return  this.Show;
   }
 
 }
