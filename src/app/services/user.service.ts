d0e026427bc3e16c9d446b956b4fbf03b7da20f5
christian <christian0092@hotmail.com>
2018-07-03 21:57:21 -0300

    Proyecto en limpio
    agregada la funcionalidad implementada anteriormente en un proyecto limpio

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,8 +2,43 @@ import { Injectable } from '@angular/core';
 import 'rxjs/Rx';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
+import { Interests } from '../models/interests';
+
+@Injectable()
+export class UserService {
+   private dummyJson:JSON; 
+   private dummyData:string='{"interest":[{"id":"4","name":"Innovacion"},{"id":"3","name":"Tecnologia"},{"id":"1","name":"Progamación"},{"id":"2","name":"Robotica"}] ,"calle":"Regimiento 3 de Caballería 473","piso":"-Departamento-","postal_code":"6400","city_id":"Trenque Lauquen","province_id":"Buenos Aires","country_id":"Argentina","cellphone":"+542392449401","name":"Jonathan Gomez","birth_date":"01/01/1876","document_type_id":"37033486","email":"borretumail@ynolose.com","job_level_id":"Empresario","study_level_id":"Universitario"}';
+   private dummyInterestList:Interests[]=[
+  new Interests('1','Progamación'),
+  new Interests('2','Robótica'),
+  new Interests('3','Tecnología'),
+  new Interests('4','Innovación')
+  ]
+
+   private userObservable$ = new Subject<JSON>();
+   private user : JSON;
+
+
+  constructor() {
+	this.dummyJson=JSON.parse(this.dummyData);
+   
+}
+ getUsername(){
+ 	return (this.dummyJson);
+ }
+  private changeUserValues(val){
+    this.user = val;
+    this.userObservable$.next(this.user);
+  }
+  getObservable$():Observable<JSON>{
+      return this.userObservable$.asObservable();
+  }
+  checkUser(){
+      this.changeUserValues(this.dummyJson);  
+  }
+}
  /*interface user {
- 		name:string,
+         name:string,
         surname:string,
         birth_date:Date,
         document_number:string,
@@ -19,37 +54,6 @@ import { Subject } from 'rxjs/Subject';
         postal_code:Int16Array
     }
     interface userInterface {
- 		name:string,
+         name:string,
        
     }*/
- 
-
-@Injectable()
-export class UserService {
-	/*DUMMY_DATA = [
-      {
-        name: 'John Lilki',
-        surname:'',
-        birth_date:'',
-        document_number:'',
-        phone:'',
-        cellphone:'',
-        email:'',
-        document_type_id:'',
-        study_level_id:'',
-        country_id:'',
-        province_id:'',
-        city_id:'',
-        user_id:'',
-        postal_code:''
-      }*/
-   private dummy_data:JSON; 
-   private test:string='{"email":"christian0092@hotmail.com"}';
-
-  constructor() {
-	this.dummy_data=JSON.parse(this.test);
-}
- getUsername(){
- 	return (this.dummy_data);
- }
-}
