348b6077b9c3d7e4cfa0dd712c6a2252bf45d709
christian <christian0092@hotmail.com>
2018-07-11 21:55:58 -0300

    m-summit
    se creo el modal del nuevo m-summit

---
+++ src/app/services/m-summit.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { FormGroup} from '@angular/forms';
+import { Headers, Http, Response } from '@angular/http';
+import 'rxjs/Rx';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class MSummitService {
+
+private header = new Headers();
+
+  constructor(private http: Http) {
+   }
+
+  send(data:FormGroup): Observable<object> {
+    this.header.append('Content-Type', 'application/json');
+  //  this.header.append('Authorization', 'Bearer ' + localStorage.getItem('userToken')['access_token']);
+    return this.http.get(
+      environment.apiUrl + 'partnerContact', { headers: this.header, search: data }
+    ).map((response: Response) => response.json());
+  }
+
+}
