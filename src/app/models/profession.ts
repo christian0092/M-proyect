bf900e31bb4609a5418024c94e150d98260c025e
Jonathan Gomez <gomez.jonathan.06@gmail.com>
2018-07-12 06:34:03 -0300

    modelos
    modelos nuevo y modificados

---
+++ src/app/models/profession.ts
@@ -0,0 +1,12 @@
+import { Deserializable } from "./deserializable.model";
+
+export class Profession implements Deserializable {
+
+    public id:string
+    public name:string
+
+    deserialize(input: any) {
+      Object.assign(this, input);
+      return this;
+    }
+}
