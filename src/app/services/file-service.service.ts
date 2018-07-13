497b5919c1c5ea254be2ce8eadf336297b1dd5e0
christian <christian0092@hotmail.com>
2018-07-13 18:35:02 -0300

    summit
    implementado download file

---
+++ src/app/services/file-service.service.ts
@@ -0,0 +1,30 @@
+
+import { Injectable } from '@angular/core';
+import { RequestOptions, ResponseContentType } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import { InterceptorService } from 'ng2-interceptors';
+import { Headers, Http, Response } from '@angular/http';
+import { environment } from '../../environments/environment';
+
+
+//import { ConfigService } from 'app/common/services/config.service';
+import { getFileNameFromResponseContentDisposition, saveFile } from '../perfil/m-summit/file-download-helper';
+
+@Injectable()
+export class FileServiceService {
+    constructor(
+       // private config: ConfigService,
+        private http: Http,
+    ) {}
+
+    downloadFile(/*propertyId: string, fileId: string*/) {
+        const url = 'http://services.mwork.com.ar/storage/app/public/summit/summit_movilidad_futura.pptx';
+        const options = new RequestOptions({responseType: ResponseContentType.Blob });
+
+        // Process the file downloaded
+        this.http.get(environment.apiUrl + 'summit/summit_movilidad_futura.pptx', options).subscribe(res => {
+            const fileName = getFileNameFromResponseContentDisposition(res);
+            saveFile(res.blob(), fileName);
+        });
+    }
+}
