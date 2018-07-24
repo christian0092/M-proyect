47cc72a50ce1c63bb92cebd5a17a1e53bc1e28ce
Jonathan Gomez <gomez.jonathan.06@gmail.com>
2018-07-24 10:19:58 -0300

    module
    quedaron los dos servicios nuevos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,13 +31,11 @@ import { VerificarCorreoService } from "./verificar-correo/verificar-correo.serv
 import {MSummitService} from "./services/m-summit.service"
 import {FileUploadClientServiceService} from "./services/file-upload-client-service.service"
 import {FileServiceService} from "./services/file-service.service"
-<<<<<<< HEAD
+
 import {PreviousRouteService} from "./services/previous-route.service"
-=======
 import {MCoffeeService} from "./services/m-coffee.service"
 /////////////////////////////////////////////////////////////////////////////
 
->>>>>>> 580a80084f5dc1526f431516b968a269d8b03680
 
 
 import { LoginComponent } from './login/login/login.component';
@@ -107,11 +105,8 @@ import { VerificarCorreoComponent } from './verificar-correo/verificar-correo.co
     MSummitService,
     FileUploadClientServiceService,
     FileServiceService,
-<<<<<<< HEAD
+    MCoffeeService,
     PreviousRouteService
-=======
-    MCoffeeService
->>>>>>> 580a80084f5dc1526f431516b968a269d8b03680
   ],
   bootstrap: [AppComponent]
 })
